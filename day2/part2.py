75c4529af012bdfc96eb35cdfd2d2135b0dd6ce9
Ruben Boone <r0785519@student.thomasmore.be>
2021-12-05 23:40:14 +0100

    day2 part 2

--- day2/part2.py
+++ day2/part2.py
@@ -1,15 +1,17 @@
 horizontal = 0
 depth = 0
+aim = 0
 
 with open("input.txt", "r") as f:
     for line in f:
         instruction = line.rstrip("\n").split(" ")
         if instruction[0] == "forward":
             horizontal += int(instruction[1])
+            depth += int(instruction[1]) * aim
         if instruction[0] == "down":
-            depth += int(instruction[1])
+            aim += int(instruction[1])
         if instruction[0] == "up":
-            depth -= int(instruction[1])
+            aim -= int(instruction[1])
 
 print(depth * horizontal)
 
