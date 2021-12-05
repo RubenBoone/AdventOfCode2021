ce07eea8424877e38f7871c5bf9194d9f0054f5a
Ruben Boone <r0785519@student.thomasmore.be>
2021-12-05 22:32:10 +0100

    day 1 part 1

---
+++ day1/main.py
@@ -0,0 +1,28 @@
+file = open("day1/input.txt", "r")
+
+notDone = True
+
+isHigher = 0
+prevItem = -1
+
+while notDone:
+    input = file.readline().rstrip("\n")
+    print(input)
+    if input == '':
+        print("end of file")
+        break
+    else:
+        input = int(input)
+
+    if prevItem == -1:
+        print("first")
+    elif input > prevItem:
+        isHigher += 1
+
+    prevItem = input
+
+print("-------")
+print(isHigher, "readings are higher")
+
+file.close()
+
